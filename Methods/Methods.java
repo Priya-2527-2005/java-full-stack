bde82eba728411968c92ae160b8f53d22ccb1d79
Priya-2527-2005 <sudanipriyavasanthi@gmail.com>
2025-06-20 12:31:14 +0530

    Add files via upload

--- Methods/Methods.java
+++ Methods/Methods.java
@@ -1,177 +1,60 @@
-/*static,instance methods and variables*/
-/*
-class Methods
-{
-    static int a=10;
-    static int b=20;
-    int c=30;
-    int d=40;
-    static void s1()
-    {
-        System.out.println("static method 1");
-    }
-    static void s2()
-    {
-         System.out.println("static method 2");
-         System.out.println(a);
-      System.out.println(b);
-    }
-     
-      void instancemethod1()
-      {
-          System.out.println("instance method1");
-      }
-      void instancemethod2()
-      {
-          System.out.println("instance method2 ");
-      }
-      public static void main(String[] args){
-          Methods a=new Methods();
-          a.s1();
-          a.s2();
-          
-          System.out.println("executed successfully"); 
-      }
-}
-static method 1
-static method 2
-10
-20
-executed successfully
-*/
-
-/*print instance variables in static method*/
+/*without parameters without return*/
 /*class Methods
 {
-    int a=10;
-    int b=20;
-    static void greet()
-    {
-        Methods obj=new Methods();
-        System.out.println(obj.a);
-        System.out.println(obj.b);
-        
-    }
-    public static void main(String[] args)
-    {
-        System.out.println("executed succesfully");
-        greet();
-    }
+public void add()
+{
+System.out.println(10+20);
 }
-executed succesfully
-10
-20*/
-
-//print staic variable in instance class//
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.add();
+}
+}*/
+/*30*/
+/*with parameters with return*/
 /*class Methods
 {
-    static int a=10;
-    static int b=20;
-    void greet()
-    {
-        System.out.println(a);
-        System.out.println(b);
-        
-    }
-    public static void main(String[] args)
-    {
-        System.out.println("hi");
-        Methods obj=new Methods();
-        obj.greet();
-        
-    }
-}*/
-/*hi
-10
-20*/
-
-/*call instance methods in static methods*/
-/*class Methods 
+public int sub(int a,int b)
 {
-    void show() 
-    {
-        System.out.println("This is instance method");
-    }
-
-    
-    public static void main(String[] args) 
-    
-    {
-        System.out.println("successful");
-        Methods obj = new Methods();
-        obj.show();  
-    }
+System.out.println(a-b);
+return(a-b);
 }
-successful
-This is instance method*/
-
-/*call static methods in instance methods*/
-/*class Methods 
+static public void main(String[] ab)
 {
-    static void show() 
-    {
-        System.out.println("This is a static method");
-    }
-
-    
-    public void display() 
-    {
-        System.out.println("Inside instance method");
-        show(); 
-    }
-
-    public static void main(String[] args) 
-    {
-        Methods obj = new Methods(); 
-        obj.display();  
-    }
+Methods m=new Methods();
+m.sub(10,5);
+}
 }*/
-/*Inside instance method
-This is a static method*/
+/*5*/
 
+/*with parameters without return*/
 /*class Methods
 {
-
-   
-    static int a = 10;
-    static int b= 20;
-
-    int c = 30;
-    int d= 40;
-
-    public static void main(String[] args) 
-    {
-       
-        Methods obj = new Methods();
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(obj.c);
-        System.out.println(obj.d);
-    }
+public void mul(int a,int b)
+{
+System.out.println(a*b);
+}
+static public void main(String[] ab)
+{
+Methods m=new Methods();
+m.mul(10,5);
+}
 }*/
-/*10
-  20
-  30
-  40*/
- 
+/*50*/
 
-/*Call static methods and instance methods in main method*/
- /*class Methods{
-    static void Method() {
-        System.out.println("This is a static method.");
-    }
-    void instanceMethod() {
-        System.out.println("This is an instance method.");
-    }
-
-    public static void main(String[] args) {
-        Method();
-        Methods obj = new Methods();
-        obj.instanceMethod();
-    }
+/*without parameters with return*/
+/*class Methods
+{
+public int div()
+{
+System.out.println(20/5);
+return(20/5);
+}
+public static void main(String[] args)
+{
+Methods m=new Methods();
+m.div();
+}
 }*/
-/*This is a static method.
-  This is an instance method.*/
-
-
-
+/*4*/
