7122eb109d79350428797bf3b4d9028102e1b683
Priya-2527-2005 <sudanipriyavasanthi@gmail.com>
2025-06-20 13:59:10 +0530

    Add files via upload

---
+++ javabasics/Basics.java
@@ -0,0 +1,98 @@
+/*create a class ,object,method,and its signature*/
+/*class Basics
+{
+   void message()
+   {
+       System.out.println("hello");
+   }
+   public static void main(String[] args)
+   {
+       Basics a=new Basics();
+       System.out.println("hi");
+       a. message();
+   }
+}/*
+/*hi
+  hello*/
+
+/*print name*/
+/*class Basics
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Priyavasanthi");
+    }
+}/*
+/*Priyavasanthi*/
+
+/*comments*/
+/*class Basics
+{
+    public static void main(String[] args)
+    {
+                System.out.println("Multiline comment");
+        
+        System.out.println("this is single comment");
+    }
+    }*/
+/*Multiline comment
+  this is single comment*/
+
+
+/*variables*/
+/*class Basics
+    {
+        public static void main(String[] args)
+        {
+            int a=10;
+            float b=20.0f;
+            boolean d=true;
+            double c= 22.22;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(d);
+            System.out.println(c);
+           }
+     }/*
+/*10
+  20.0
+  true
+  22.22*/
+
+
+/*global and local*/
+/*class Basics
+{
+    int globalVar = 5; 
+
+    void display() 
+    {
+        int localVar = 10;
+        System.out.println("Global: " + globalVar);
+        System.out.println("Local: " + localVar);
+    }
+
+    public static void main(String[] args) 
+{
+        Basics t = new Basics();
+        t.display();
+    }
+}*/
+/*Global: 5
+  Local: 10*/
+
+
+/*print your name and and call the function from main method*/
+/*class Basics
+{
+        static void name()
+        {
+            System.out.println("Priyavasanthi");
+        }
+        public static void main(String[] args)
+        {
+            name();
+        }
+    }*/
+/*Priyavasanthi*/
+
